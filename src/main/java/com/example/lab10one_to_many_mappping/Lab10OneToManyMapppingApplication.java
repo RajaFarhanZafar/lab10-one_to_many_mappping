136ebd0bfcb18851ab71968dbe7a0f75287fbe53
Raja Farhan Zafar <bcs163997@cust.pk>
2020-05-09 16:16:49 +0500

    lab_10-One-To-Many-Mapping

---
+++ src/main/java/com/example/lab10one_to_many_mappping/Lab10OneToManyMapppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab10one_to_many_mappping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab10OneToManyMapppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab10OneToManyMapppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab10one_to_many_mappping/Lab10OneToManyMapppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab10one_to_many_mappping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10OneToManyMapppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
